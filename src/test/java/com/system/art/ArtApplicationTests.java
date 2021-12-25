a48b91200eb2f3d598a29fccb6c18f56cef04608
lourong <1422084460@qq.com>
2021-12-25 16:11:11 +0800

    添加rmq模块

--- src/test/java/com/system/art/ArtApplicationTests.java
+++ src/test/java/com/system/art/ArtApplicationTests.java
@@ -1,13 +1,19 @@
 package com.system.art;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ArtApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
+    @Test
+    void FanoutProvider() {
+        rabbitTemplate.convertAndSend("my_fanout","","msg ok");
+        //rabbitTemplate.convertAndSend("hello","direct model");
+    }
 }
