42a95ca9badea3a8cc698f131cd33d014a65659d
lourong <1422084460@qq.com>
2021-12-26 14:04:07 +0800

    删除不需要的Store2

--- src/main/java/com/system/art/AppStart.java
+++ src/main/java/com/system/art/AppStart.java
@@ -1,7 +1,6 @@
 package com.system.art;
 
 import com.system.art.po.Store;
-import com.system.art.po.Store2;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
